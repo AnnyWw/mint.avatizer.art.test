db028ada61b3f8f7efbfb7e5bd36a05c489592f5
Evgeniy Tkachenko <webmaster@electronic.us>
2022-10-06 18:20:40 +0300

    remove vontract id to config

--- ang-mintsite/src/environments/environment.prod.ts
+++ ang-mintsite/src/environments/environment.prod.ts
@@ -5,5 +5,7 @@ export const environment = {
   openseaUrl:"https://opensea.io",
   twitterUrl:"https://twitter.com/Avatizer_NFT",
   discordUrl:"https://discord.gg/avatizer",
-	siteName:"Avatizer"
+  siteName:"Avatizer",
+  contractAv:"0x5d74387c391b88c35425d0ec9f82750562fc173f",
+  walletconnect_infuraId:"68bbfa6dd6594f328012419c5b654b2f"
 };
