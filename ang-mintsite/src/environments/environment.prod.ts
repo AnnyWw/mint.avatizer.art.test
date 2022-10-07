f8c1e7492977866573980e893cbd38204aae74c6
Vit <68844347+coherentkite@users.noreply.github.com>
2022-10-07 12:49:03 +0200

    updated contract address

--- ang-mintsite/src/environments/environment.prod.ts
+++ ang-mintsite/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  token:"jdjaosiij28892SIAHI049923701CIGknlklajal",
-  mainsiteUrl:"https://www.avatizer.art",
-  openseaUrl:"https://opensea.io",
-  twitterUrl:"https://twitter.com/Avatizer_NFT",
-  discordUrl:"https://discord.gg/avatizer",
-  siteName:"Avatizer",
-  contractAv:"0x5d74387c391b88c35425d0ec9f82750562fc173f",
-  walletconnect_infuraId:"68bbfa6dd6594f328012419c5b654b2f"
+  token: 'jdjaosiij28892SIAHI049923701CIGknlklajal',
+  mainsiteUrl: 'https://www.avatizer.art',
+  openseaUrl: 'https://opensea.io',
+  twitterUrl: 'https://twitter.com/Avatizer_NFT',
+  discordUrl: 'https://discord.gg/avatizer',
+  siteName: 'Avatizer',
+  contractAv: '0xBA0083C67EE14e93F385EDaf9eFCfFD47e883c9e',
+  walletconnect_infuraId: '68bbfa6dd6594f328012419c5b654b2f',
 };
