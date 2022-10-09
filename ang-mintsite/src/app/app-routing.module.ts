66cd74f594f2ffbd5f0836c3f5823765d85cffb7
Demon <dmitriy.movchan@electronic.us>
2022-10-09 18:30:34 +0300

    save

--- ang-mintsite/src/app/app-routing.module.ts
+++ ang-mintsite/src/app/app-routing.module.ts
@@ -4,13 +4,41 @@ import { ManageNFTComponent } from './pages/manage-nft/manage-nft.component';
 import { MintComponent } from './pages/mint/mint.component';
 
 const routes: Routes = [
-  { path: "", redirectTo: "mint", pathMatch: "full" },
-  { path: "mint", component: MintComponent },
-  { path: "manageNFT", component: ManageNFTComponent }
+  { path: '', redirectTo: 'mint', pathMatch: 'full' },
+  {
+    path: 'mint',
+    component: MintComponent,
+    title: 'Avatizer - Mint',
+    data: {
+      seo: {
+        metaTags: [
+          {
+            name: 'description',
+            content: 'Avatizer NFT - Free Mint. One NFT per wallet.',
+          },
+        ],
+      },
+    },
+  },
+  {
+    path: 'manageNFT',
+    component: ManageNFTComponent,
+    title: 'Avatizer - Manage my collection',
+    data: {
+      seo: {
+        metaTags: [
+          {
+            name: 'description',
+            content: 'Manage my Avatizers. Pausing or restoring daily reloads.',
+          },
+        ],
+      },
+    },
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
