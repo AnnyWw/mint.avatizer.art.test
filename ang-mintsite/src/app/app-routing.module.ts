cd2d26fb40e722d5248fb7d4e05827cee94351a2
Anastasiia <anny.wayward@gmail.com>
2022-10-10 21:32:22 +0300

    fix

--- ang-mintsite/src/app/app-routing.module.ts
+++ ang-mintsite/src/app/app-routing.module.ts
@@ -5,10 +5,52 @@ import { MintComponent } from './pages/mint/mint.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 const routes: Routes = [
-  { path: "", redirectTo: "mint", pathMatch: "full" },
-  { path: "mint", component: MintComponent },
-  { path: "manageNFT", component: ManageNFTComponent },
-  { path: '404', component: NotFoundComponent },
+  { path: '', redirectTo: 'mint', pathMatch: 'full' },
+  {
+    path: 'mint',
+    component: MintComponent,
+    title: 'Avatizer - Mint',
+    data: {
+      seo: {
+        metaTags: [
+          {
+            name: 'description',
+            content: 'Avatizer NFT - Free Mint. One NFT per wallet.',
+          },
+        ],
+      },
+    },
+  },
+  {
+    path: 'manageNFT',
+    component: ManageNFTComponent,
+    title: 'Avatizer - Manage my collection',
+    data: {
+      seo: {
+        metaTags: [
+          {
+            name: 'description',
+            content: 'Manage my Avatizers. Pausing or restoring daily reloads.',
+          },
+        ],
+      },
+    },
+  },
+  { 
+  path: '404', 
+  component: NotFoundComponent,
+  title: 'Avatizer - Page Not Found 404',
+    data: {
+      seo: {
+        metaTags: [
+          {
+            name: 'description',
+            content: 'We are sorry, but the page you are currently looking for is not found.',
+          },
+        ],
+      },
+    },
+  },
   { path: '**', redirectTo: '/404' }
 ];
 
