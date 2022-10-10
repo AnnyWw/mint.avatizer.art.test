0e081d47befc6aa88206128a2397a2a3ef3fd9d2
Anastasiia <anny.wayward@gmail.com>
2022-10-10 18:57:19 +0300

    fix parallax

--- ang-mintsite/src/app/pages/mint/mint.component.ts
+++ ang-mintsite/src/app/pages/mint/mint.component.ts
@@ -70,6 +70,48 @@ export class MintComponent implements OnInit {
     $('#connect').click(function () {
       console.log('here');
     });
+    
+  }
+
+  startParallax() {
+    $(document).ready(function () {
+    $('.img-parallax').each(function(){
+        var img = $(this);
+        var imgParent = $(this).parent();
+        function parallaxImg () {
+            var speed = img.data('speed');
+            var imgY = imgParent.offset().top;
+            var winY = $(window).scrollTop();
+            var winH = $(window).height();
+            var parentH = imgParent.innerHeight();
+
+
+            // The next pixel to show on screen      
+            var winBottom = winY + winH;
+
+            // If block is shown on screen
+            if (winBottom > imgY && winY < imgY + parentH) {
+            // Number of pixels shown after block appear
+            var imgBottom = ((winBottom - imgY) * speed);
+            // Max number of pixels until block disappear
+            var imgTop = winH + parentH;
+            // Porcentage between start showing until disappearing
+            var imgPercent = ((imgBottom / imgTop) * 100) + (50 - (speed * 50));
+            }
+            img.css({
+            top: imgPercent + '%',
+            transform: 'translateY(-' + imgPercent + '%)'
+            });
+        }
+        $(document).on({
+            scroll: function () {
+            parallaxImg();
+            }, ready: function () {
+            parallaxImg();
+            }
+        });
+    });
+    });
   }
 
   async initWeb3() {
@@ -255,6 +297,7 @@ export class MintComponent implements OnInit {
     }
 
     this.status;
+    this.startParallax();
   }
    closeNotify() {
     this.isShow = !this.isShow;
