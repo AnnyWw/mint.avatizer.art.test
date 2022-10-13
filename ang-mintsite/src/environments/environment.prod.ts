30399da10883ded9e411437beabe414bdb977619
Vit <68844347+coherentkite@users.noreply.github.com>
2022-10-13 10:19:39 +0200

    contract address

--- ang-mintsite/src/environments/environment.prod.ts
+++ ang-mintsite/src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
-  minting_status: "start", //not_start | start | stop  
+  minting_status: 'start', //not_start | start | stop
   token: 'jdjaosiij28892SIAHI049923701CIGknlklajal',
   mainsiteUrl: 'https://www.avatizer.art',
   openseaUrl: 'https://opensea.io',
   twitterUrl: 'https://twitter.com/Avatizer_NFT',
   discordUrl: 'https://discord.gg/avatizer',
   siteName: 'Avatizer',
-  contractAv: '0xBA0083C67EE14e93F385EDaf9eFCfFD47e883c9e',
+  contractAv: '0x241981C4Cc9901BF01234649DcbD3188dd1E71bf',
   walletconnect_infuraId: '68bbfa6dd6594f328012419c5b654b2f',
-    minting_date_start:'Tue, Oct 18, 2022 @ 11:00 AM EST (NYC Time)'
+  minting_date_start: 'Tue, Oct 18, 2022 @ 11:00 AM EST (NYC Time)',
 };
