ef0f1bfd96f03a6e33c5d0c4b341b08c87efed6e
Evgeniy Tkachenko <webmaster@electronic.us>
2022-10-17 00:50:50 +0300

    fix   walletconnect_infuraId: '68bbfa6dd6594f328012419c5b654b2f',//'5cefff2052fb40bd93b90658d9949089',

--- ang-mintsite/src/environments/environment.prod.ts
+++ ang-mintsite/src/environments/environment.prod.ts
@@ -8,7 +8,7 @@ export const environment = {
   discordUrl: 'https://discord.gg/avatizer',
   siteName: 'Avatizer',
   contractAv: '0x41C28eb53d68ae62eA47656Ce6DeC63e2327D2EA',
-  walletconnect_infuraId: '5cefff2052fb40bd93b90658d9949089',
+  walletconnect_infuraId: '68bbfa6dd6594f328012419c5b654b2f',//'5cefff2052fb40bd93b90658d9949089',
   minting_date_start: 'Tue, Oct 18, 2022 @ 11:00 AM EST (NYC Time)',
   moralis_api_key:
     '8kEmud1tbZttJ6IZdoVSejGjUt4NXlbJXjLqEu5HsjRezZYkXZv1pEX6BslaWQ8T',
